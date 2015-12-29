055e64bf3b0747fdca12eac85610fb11230f7942
afon <wengxufeng@gmail.com>
2015-12-29 16:22:46 +0800

    remove card exception

--- src/com/pingplusplus/model/CardCollection.java
+++ src/com/pingplusplus/model/CardCollection.java
@@ -3,13 +3,12 @@ package com.pingplusplus.model;
 import com.pingplusplus.Pingpp;
 import com.pingplusplus.exception.*;
 
-import javax.smartcardio.CardException;
 import java.util.Map;
 
 public class CardCollection extends PingppCollectionAPIResource<Card> {
 
     public Card retrieve(String id) throws AuthenticationException,
-            InvalidRequestException, APIConnectionException, CardException,
+            InvalidRequestException, APIConnectionException,
             APIException, ChannelException {
         String url = String.format("%s%s/%s", Pingpp.getApiBase(), this.getURL(), id);
         return request(RequestMethod.GET, url, null, Card.class);
