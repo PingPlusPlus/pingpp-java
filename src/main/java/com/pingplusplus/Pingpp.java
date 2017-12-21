4e8c755094e8b9cc6597270e472eac081a366016
dong11 <956176457@qq.com>
2017-12-21 17:52:36 +0800

    兼容 JDK 1.7

--- src/main/java/com/pingplusplus/Pingpp.java
+++ src/main/java/com/pingplusplus/Pingpp.java
@@ -11,7 +11,7 @@ public abstract class Pingpp {
     /**
      * version
      */
-    public static final String VERSION = "2.3.1";
+    public static final String VERSION = "2.3.2";
     /**
      * api key
      */
